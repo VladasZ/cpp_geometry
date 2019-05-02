90136a032d78ce518f72fe5f18973e000fd83eea
Vladas <146100@gmail.com>
2019-05-02 13:06:31 +0300

    added vertices

---
+++ source/3D/vertices/Vertex.hpp
@@ -0,0 +1,32 @@
+//
+//  Vertex.hpp
+//  geometry
+//
+//  Created by Vladas Zakrevskis on 2/27/19.
+//  Copyright © 2019 VladasZ. All rights reserved.
+//
+
+#pragma once
+
+#include "Vector3.hpp"
+
+namespace gm {
+
+class Vertex {
+
+public:
+
+    using Index   = unsigned short;
+    using Indices = std::vector<Index>;
+    using Array   = std::vector<Vertex>;
+
+    Vector3 position;
+    Vector3 normal;
+
+    Vertex() = default;
+    Vertex(const Vector3& position, const Vector3& normal);
+
+    std::string to_string() const;
+};
+
+}
