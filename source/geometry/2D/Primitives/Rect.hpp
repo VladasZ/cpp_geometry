056f106b52a9f88170349d6624438aea35a6941e
Vladas <146100@gmail.com>
2020-04-26 06:19:23 +0300

    ...

--- source/geometry/2D/Primitives/Rect.hpp
+++ source/geometry/2D/Primitives/Rect.hpp
@@ -16,54 +16,54 @@
 
 namespace gm {
 
-enum class Edge {
-    Top         = 0b00000001,
-    Bottom      = 0b00000010,
-    Left        = 0b00000100,
-    Right       = 0b00001000,
-    CenterV     = 0b00010000,
-    CenterH     = 0b00100000,
-    Center      = CenterV | CenterH,
-    TopLeft     = Top     | Left,
-    TopRight    = Top     | Right,
-    BottomLeft  = Bottom  | Left,
-    BottomRight = Bottom  | Right,
-    None        = 0
-};
+    enum class Edge {
+        Top         = 0b00000001,
+        Bottom      = 0b00000010,
+        Left        = 0b00000100,
+        Right       = 0b00001000,
+        CenterV     = 0b00010000,
+        CenterH     = 0b00100000,
+        Center      = CenterV | CenterH,
+        TopLeft     = Top     | Left,
+        TopRight    = Top     | Right,
+        BottomLeft  = Bottom  | Left,
+        BottomRight = Bottom  | Right,
+        None        = 0
+    };
 
-class Rect {
+    class Rect {
 
-public:
+    public:
 
-    Point origin;
-    Size  size;
+        Point origin;
+        Size  size;
 
-    Rect() = default;
-    Rect(const Size&);
-    Rect(Float size);
-    Rect(Float width, Float height);
-    Rect(Float x, Float y, Float width, Float height);
+        Rect() = default;
+        Rect(const Size&);
+        Rect(Float size);
+        Rect(Float width, Float height);
+        Rect(Float x, Float y, Float width, Float height);
 
-    void set_edge(Edge, Point);
+        void set_edge(Edge, Point);
 
-    Float min_x() const;
-    Float min_y() const;
-    Float max_x() const;
-    Float max_y() const;
+        Float min_x() const;
+        Float min_y() const;
+        Float max_x() const;
+        Float max_y() const;
 
-    Point center() const;
-    void set_center(const Point&);
+        Point center() const;
+        void set_center(const Point&);
 
-    bool contains(const Point&) const;
-    bool contains_with_edge(const Point&, Float) const;
+        bool contains(const Point&) const;
+        bool contains_with_edge(const Point&, Float) const;
 
-    Size fit_size(const Size&) const;
+        Size fit_size(const Size&) const;
 
-    Rect with_zero_origin() const;
+        Rect with_zero_origin() const;
 
-    std::array<Rect, 4> edges() const;
+        std::array<Rect, 4> edges() const;
 
-    std::string to_string() const;
-};
+        std::string to_string() const;
+    };
 
 }
