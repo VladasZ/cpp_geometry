4da79e27bf47b177c69766f627dff4b3b9aa3da7
Vladas Zakrevskis <146100@gmail.com>
2020-10-08 18:06:32 +0300

    quaternion multiplication test

--- source/geometry_test/main.cpp
+++ source/geometry_test/main.cpp
@@ -3,7 +3,17 @@
 #include "Matrix4.hpp"
 
 int main() {
-    auto mat = gm::Matrix4::transform::scale(10.0f);
-    Logvar(mat);
+
+    auto spes  = gm::Vector4::transform::quaternion_rotating_x(1);
+    auto spes2 = gm::Vector4::transform::quaternion_rotating_y(0.5);
+
+    Log << spes;
+    Log << spes.length();
+
+    auto spes3 = spes * spes2;
+
+    Log << spes3;
+    Log << spes3.length();
+
     return 0;
 }
