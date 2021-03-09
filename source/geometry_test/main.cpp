b6fbd3e6b9cecc615bc1a4b67cd9a07afd6a88c3
Vladas Zakrevskis <146100@gmail.com>
2021-03-09 13:06:56 +0300

    added mapping

--- source/geometry_test/main.cpp
+++ source/geometry_test/main.cpp
@@ -1,12 +1,12 @@
 
 #include "Log.hpp"
-#include "Matrix4.hpp"
+#include "GmMapping.hpp"
 
 using namespace gm;
 
 int main() {
 
-   // Log << json_mapper.to_json(Rect(345, 434, 17, 434));
+    Log << json_mapper.to_json(Rect(345, 434, 17, 434));
 
     return 0;
 }
